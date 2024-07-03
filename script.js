b2d1bc4ffc33a09d6a8efb0a0f42e1a75d9a6c59
ManishaGupta83 <manishaguptaa011@gmail.com>
2024-07-03 12:43:43 +0530

    button color changes done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let mybutton1 = document.getElementById("btn1");
 let mybutton2 = document.getElementById("btn2");
 let copycode = document.querySelector(".color");
 
+
+
 let myhexValues = () => {
   let myHexagon = "0123456789abcdef";
   let colors = "#";
@@ -10,22 +12,32 @@ let myhexValues = () => {
   }
   return colors;
 };
-let rgb1 = "#000";
-let rgb2 = "#fff";
+
+
+
+let rgb1 = "#004773";
+let rgb2 = "#54d542";
+
 const handleButton1 = () => {
   rgb1 = myhexValues();
   console.log(rgb1);
   mybutton1.innerText=rgb1;
   document.body.style.backgroundImage = `linear-gradient(to right ,${rgb1},${rgb2}`;
-  copycode.innerHTML = `background-image:linear-gradient(to right, ${rgb1},${rgb2});`;
+  copycode.innerHTML = `background-image:linear-gradient(to right, ${rgb1},${rgb2})`;
 };
+
+
 const handleButton2 = () => {
   rgb2 = myhexValues();
   console.log(rgb2);
   mybutton2.innerText=rgb2;
   document.body.style.backgroundImage = `linear-gradient(to right ,${rgb1},${rgb2}`;
-  copycode.innerHTML = `background-image:linear-gradient(to right, ${rgb1},${rgb2});`;
+  copycode.innerHTML = `background-image:linear-gradient(to right, ${rgb1},${rgb2})`;
 };
+
+copycode.addEventListener("click",()=>{
+  navigator.clipboard.writeText(copycode.innerHTML);
+});
 
 mybutton1.addEventListener("click", handleButton1);
 mybutton2.addEventListener("click", handleButton2);
